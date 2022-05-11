67bd4dde860ab15193a8424c6c8c5dbf33d319a5
hackmos <srijanpanta@email.com>
2022-05-11 20:49:57 +0545

    Migrations added

---
+++ database/migrations/2022_05_07_084740_create_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string("reportName");
+            $table->string("reportDescription");
+            $table->string("diseaseName");
+            $table->string("location");
+            $table->string("reportImage");
+            $table->unsignedBigInteger("userId");
+            $table->foreign('userId')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+};
