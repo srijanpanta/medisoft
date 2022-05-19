10c0f3581f4df45ce3ffb1c943e42d1007f293c9
hackmos <srijanpanta@email.com>
2022-05-19 19:49:36 +0545

    Factory added for reports

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Districts;
+use App\Models\User;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Report>
+ */
+class ReportFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'reportName' => $this->faker->text(10),
+            'reportDescription' => $this->faker->text(200),
+            'diseaseName' =>  $this->faker->randomElement(['Allergies', 'Colds and Flu', 'Conjunctivitis', 'Diarrhea', 'Headaches', 'Mononucleosis', 'Stomach Aches', 'Covid', 'Kidney Stone', 'Diabetes', 'Depression', 'Anxiety', 'Hemorrhoid', 'Yeast infection', 'Lupus', 'Shingles', 'Psoriasis', 'Schizophrenia', 'Lyme disease', 'HPV', 'Herpes', 'Pneumonia', 'Fibromyalgia', 'Scabies', 'Chlamydia', 'Endometriosis', 'Strep throat', 'Diverticulitis', 'Bronchitis']),
+            'reportImage'=>'sampleReport.png',
+            'location'=> Districts::all()->random()->id,
+            'userId' => User::all()->random()->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
