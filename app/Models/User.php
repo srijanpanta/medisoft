4b9ab20a8d04237fd0db07da794f6919061d2275
hackmos <srijanpanta@email.com>
2022-05-21 21:07:15 +0545

    Models updated to add functionality of friend request

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,93 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Report::class);
     }
+    //friendship that this user started
+	public function friendsOfThisUser()
+	{
+		return $this->belongsToMany(User::class, 'friendships', 'first_user', 'second_user')
+		->withPivot('status')
+		->wherePivot('status', 'confirmed');
+	}
+
+	// friendship that this user was asked for
+	public function thisUserFriendOf()
+	{
+		return $this->belongsToMany(User::class, 'friendships', 'second_user', 'first_user')
+		->withPivot('status')
+		->wherePivot('status', 'confirmed');
+	}
+
+	// accessor allowing you call $user->friends
+	public function getFriendsAttribute()
+	{
+		if ( ! array_key_exists('friends', $this->relations)) $this->loadFriends();
+		return $this->getRelation('friends');
+	}
+
+	public function loadFriends()
+	{
+		if ( ! array_key_exists('friends', $this->relations))
+		{
+		$friends = $this->mergeFriends();
+		$this->setRelation('friends', $friends);
+	}
+	}
+
+	public function mergeFriends()
+	{
+		if($temp = $this->friendsOfThisUser)
+		return $temp->merge($this->thisUserFriendOf);
+		else
+		return $this->thisUserFriendOf;
+	}
+//======================== end functions to get friends attribute =========================
+//====================== functions to get blocked_friends attribute ============================
+
+	// friendship that this user started but now blocked
+	public function friendsOfThisUserBlocked()
+	{
+		return $this->belongsToMany(User::class, 'friendships', 'first_user', 'second_user')
+					->withPivot('status', 'acted_user')
+					->wherePivot('status', 'blocked')
+					->wherePivot('acted_user', 'first_user');
+	}
+
+	// friendship that this user was asked for but now blocked
+	public function thisUserFriendOfBlocked()
+	{
+		return $this->belongsToMany(User::class, 'friendships', 'second_user', 'first_user')
+					->withPivot('status', 'acted_user')
+					->wherePivot('status', 'blocked')
+					->wherePivot('acted_user', 'second_user');
+	}
+
+	// accessor allowing you call $user->blocked_friends
+	public function getBlockedFriendsAttribute()
+	{
+		if ( ! array_key_exists('blocked_friends', $this->relations)) $this->loadBlockedFriends();
+			return $this->getRelation('blocked_friends');
+	}
+
+	public function loadBlockedFriends()
+	{
+		if ( ! array_key_exists('blocked_friends', $this->relations))
+		{
+			$friends = $this->mergeBlockedFriends();
+			$this->setRelation('blocked_friends', $friends);
+		}
+	}
+
+	public function mergeBlockedFriends()
+	{
+		if($temp = $this->friendsOfThisUserBlocked)
+			return $temp->merge($this->thisUserFriendOfBlocked);
+		else
+			return $this->thisUserFriendOfBlocked;
+	}
+// ======================================= end functions to get block_friends attribute =========
+public function friend_requests()
+{
+	return $this->hasMany(Friendship::class, 'second_user')
+	->where('status', 'pending');
+}
 }
