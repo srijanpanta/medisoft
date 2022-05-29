7af2db8645e9cf32193caabcd9588e3fa9831030
hackmos <srijanpanta@email.com>
2022-05-29 20:03:56 +0545

    Routes added for admin

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\FriendshipController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\DiseaseController;
+use App\Http\Controllers\AdminController;
 
 
 
@@ -34,23 +35,21 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::put('/home',[App\Http\Controllers\HomeController::class, 'update'])->name('home.update');
 
 Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
-
-Route::get('location-user', [LocationController::class, 'index']);
-Route::resource('reports', ReportController::class)->middleware('auth');
-Route::resource('doctors', DoctorController::class)->middleware('auth');
-
+Route::resource('reports', ReportController::class)->middleware('auth')->middleware('checkStatus');
+Route::resource('doctors', DoctorController::class);
 
 Route::get('change-password', [ChangePasswordController::class,'index'])->name('changePassword.index');
 Route::post('change-password', [ChangePasswordController::class,'store'])->name('changePassword.update');
 
-Route::resource('friends',FriendshipController::class)->middleware('auth');
+Route::resource('friends',FriendshipController::class)->middleware('auth')->middleware('checkStatus');
 
 Route::get('checkup',function(){
     return view('checkup');
-})->name('checkup');
+})->name('checkup')->middleware('auth')->middleware('checkStatus');
 
 Route::get('send', [App\Http\Controllers\HomeController::class, 'sendNotification']);
 Route::get('friendRequestNotification', [App\Http\Controllers\HomeController::class, 'friendRequestNotification'])->name('friendRequestNotification');
+Route::get('sendAcceptedNotification', [App\Http\Controllers\HomeController::class, 'sendAcceptedNotification'])->name('sendAcceptedNotification');
 Route::get('notifications', [App\Http\Controllers\HomeController::class, 'getNotifications'])->name('notifications');
 Route::get('readMsg', [App\Http\Controllers\HomeController::class, 'readNotifications'])->name('readMsg');
 Route::get('readMsgSingle/{notificationId}', [App\Http\Controllers\HomeController::class, 'readNotificationsSingle'])->name('readMsgSingle');
@@ -59,3 +58,10 @@ Route::post('newsletter', NewsletterController::class);
 
 Route::get('disease',[DiseaseController::class,'index'])->name('disease');
 Route::get('disease\{diseaseName}',[DiseaseController::class,'place'])->name('disease.place');
+
+Route::group(['middleware' => 'can:isAdmin'], function () {
+        Route::get('newdoctors',[AdminController::class,'newDoctor'])->name('newDoctors');
+        Route::get('showdoctor/{doctor}',[AdminController::class,'showDoctor'])->name('showDoctor');
+        Route::put('acceptdoctor/{doctor}',[AdminController::class,'acceptDoctor'])->name('acceptDoctor');
+
+});
