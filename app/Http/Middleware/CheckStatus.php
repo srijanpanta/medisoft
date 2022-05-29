0331bcd148e2e9d3ba7f10ff82c83c0063531c0e
hackmos <srijanpanta@email.com>
2022-05-29 20:00:30 +0545

    Middleware added to check verifcation status

---
+++ app/Http/Middleware/CheckStatus.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+       if($request->user()->status!='verified')
+       {
+           return redirect()->back()->with('warning','Your account is not verified yet');
+       }
+       else{
+           return $next($request);
+       }
+    }
+}
